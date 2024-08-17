0121fb7f53e42324cb7183b6bb82fa725abeacce
yanikdiallo <yayadiallo2012dev@gmail.com>
2024-08-17 16:34:34 +0000

    ajout du graphe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd
+import plotly.express as px
 
 st.title("Graphe DE Temperature ")
 place = st.text_input("Place: ")
@@ -8,3 +9,13 @@ days = st.slider("Forecast days ",min_value=1,max_value=5,help="Select the numbe
 
 option = st.selectbox("Select data to view ",("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+def get_data(days):
+    dates =["2022-25-10","2022-26-10","2022-27-10"]
+    temperatures =[10,11,15]
+    temperatures = [days*i for i in temperatures]
+    return dates, temperatures
+d ,t=get_data(days)
+
+figure =px.line(x=d,y=t,labels={"x":"Date","y":"Temperatures"})
+st.plotly_chart(figure)
+
